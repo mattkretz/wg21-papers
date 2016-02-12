9b72ee72d37ba1671a5d2c715f5762b4c7634d98
Matthias Kretz <kretz@kde.org>
2016-02-12 17:02:51 +0100

    More mask functions/operators

--- synopsis.cpp
+++ synopsis.cpp
@@ -10,6 +10,13 @@ namespace std {
     struct unaligned_tag {};
     struct aligned_tag {};
 
+    // traits [datapar.traits]
+    template <class T> struct is_datapar;
+    template <class T> constexpr bool is_datapar_v = is_datapar<T>::value;
+
+    template <class T> struct is_mask;
+    template <class T> constexpr bool is_mask_v = is_mask<T>::value;
+
     template <class T, size_t N> struct abi_for_size { typedef implementation_defined type; };
     template <size_t N> using abi_for_size_t = typename abi_for_size<N>::type;
 
@@ -18,10 +25,13 @@ namespace std {
     template <class T, class Abi = datapar_abi::compatible>
     constexpr size_t datapar_size_v = datapar_size<T, Abi>::value;
 
+    // class template datapar [datapar]
     template <class T, class Abi = datapar_abi::compatible> class datapar;
+
+    // class template mask [mask]
     template <class T, class Abi = datapar_abi::compatible> class mask;
 
-    // compound assignment
+    // compound assignment [datapar.cassign]
     template <class T, class Abi, class U> datapar<T, Abi> &operator+= (datapar<T, Abi> &, const U &);
     template <class T, class Abi, class U> datapar<T, Abi> &operator-= (datapar<T, Abi> &, const U &);
     template <class T, class Abi, class U> datapar<T, Abi> &operator*= (datapar<T, Abi> &, const U &);
@@ -33,6 +43,7 @@ namespace std {
     template <class T, class Abi, class U> datapar<T, Abi> &operator<<=(datapar<T, Abi> &, const U &);
     template <class T, class Abi, class U> datapar<T, Abi> &operator>>=(datapar<T, Abi> &, const U &);
 
+    // binary operators [datapar.binary]
     template <class L, class R> using datapar_return_type = ...;  // exposition only
 
     template <class T, class Abi, class U>
@@ -76,6 +87,7 @@ namespace std {
     template <class T, class Abi, class U>
     datapar_return_type<datapar<T, Abi>, U> operator>>(const U &, datapar<T, Abi>);
 
+    // compares [datapar.comparison]
     template <class T, class Abi, class U>
     typename datapar_return_type<datapar<T, Abi>, U>::mask_type operator==(datapar<T, Abi>, const U &);
     template <class T, class Abi, class U>
@@ -101,18 +113,29 @@ namespace std {
     template <class T, class Abi, class U>
     typename datapar_return_type<datapar<T, Abi>, U>::mask_type operator< (const U &, datapar<T, Abi>);
 
-    template <class T> struct is_datapar;
-    template <class T> constexpr bool is_datapar_v = is_datapar<T>::value;
-
+    // casts [datapar.casts]
     template <class T, class U, class... Us>
     conditional_t<(T::size() == (U::size() + Us::size()...)), T,
                   array<T, (U::size() + Us::size()...) / T::size()>> datapar_cast(U, Us...);
 
-    template <class T, class Abi> bool  all_of(mask<T, Abi>);
-    template <class T, class Abi> bool  any_of(mask<T, Abi>);
-    template <class T, class Abi> bool none_of(mask<T, Abi>);
-    template <class T, class Abi> bool some_of(mask<T, Abi>);
+    // mask compares [mask.comparison]
+    template <class T, class Abi, class U> bool operator==(mask<T, Abi>, const U &);
+    template <class T, class Abi, class U> bool operator!=(mask<T, Abi>, const U &);
+    template <class T, class Abi, class U> bool operator==(const U &, mask<T, Abi>);
+    template <class T, class Abi, class U> bool operator!=(const U &, mask<T, Abi>);
 
-    template <class T, class U, class Abi> implementation_defined where(mask<T, Abi>, datapar<U, Abi> &);
+    // reductions [mask.reductions]
+    template <class T, class Abi> bool  all_of(mask<T, Abi>);
+    constexpr bool  all_of(bool);
+    template <class T, class Abi> bool  any_of(mask<T, Abi>);
+    constexpr bool  any_of(bool);
+    template <class T, class Abi> bool none_of(mask<T, Abi>);
+    constexpr bool none_of(bool);
+    template <class T, class Abi> bool some_of(mask<T, Abi>);
+    constexpr bool some_of(bool);
+
+    // masked assignment [mask.where]
+    template <class T, class U, class Abi> implementation_defined where(mask<U, Abi>, datapar<T, Abi> &);
+    template <class T> implementation_defined where(bool, T &);
   }
 }
