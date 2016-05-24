6335e9a88b523adbd3d6b56dcc895dd1faa2360c
Matthias Kretz <kretz@kde.org>
2016-05-24 17:14:54 +0200

    Fix example code & picture
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- DMK02/foreach.cpp
+++ DMK02/foreach.cpp
@@ -1,19 +1,30 @@
-void for_each(DataparPolicy, ContiguousIterator first, ContiguousIterator last,
-              UnaryFunction f) {
-  using V = datapar<ContiguousIterator::value_type>;
-  using V1 = datapar<ContiguousIterator::value_type, datapar_abi::scalar>;
+template <size_t N>
+void epilogue(ContiguousIterator first, ContiguousIterator last, UnaryFunction f);
+
+template <>
+inline void epilogue<0>(ContiguousIterator, ContiguousIterator, UnaryFunction) {}
+
+template <size_t N>
+inline void epilogue(ContiguousIterator first, ContiguousIterator last,
+                     UnaryFunction f) {
+  using V = datapar<ContiguousIterator::value_type, abi_for_size_t<N>>;
+  V tmp = load<V>(std::addressof(*first), flags::unaligned);
+  f(tmp);
+  if (is_functor_argument_mutable<UnaryFunction, V>::value) {
+    store(tmp, std::addressof(*first), flags::unaligned);
+  }
+  epilogue<V::size() / 2>(first, last, f);
+}
+
+void for_each(/*!\dataparEPT            {}*/, ContiguousIterator first,
+              ContiguousIterator last, UnaryFunction f) {
+  using V = datapar<ContiguousIterator::value_type, datapar_abi::native>;
   for (; distance(first, last) >= V::size(); first += V::size()) {
     V tmp = load<V>(std::addressof(*first), flags::unaligned);
     f(tmp);
-    if (Traits::is_functor_argument_mutable<UnaryFunction, V>::value) {
+    if (is_functor_argument_mutable<UnaryFunction, V>::value) {
       store(tmp, std::addressof(*first), flags::unaligned);
     }
   }
-  for (; first != last; ++first) {
-    V1 tmp =load<V1>(std::addressof(*first), flags::unaligned);
-    f(tmp);
-    if (Traits::is_functor_argument_mutable<UnaryFunction, V1>::value) {
-      store(tmp, std::addressof(*first), flags::unaligned);
-    }
-  }
+  epilogue<V::size() / 2>(first, last, f);
 }
--- DMK02/iota.cpp
+++ DMK02/iota.cpp
@@ -1,29 +1,30 @@
 template <size_t N>
-epilogue(ContiguousIterator first, ContiguousIterator last,
-         ContiguousIterator::value_type first_value);
+void epilogue(ContiguousIterator first, ContiguousIterator last,
+              ContiguousIterator::value_type first_value);
+
 template <>
-inline void epilogue<1>(ContiguousIterator first, ContiguousIterator,
-                        ContiguousIterator::value_type first_value) {
-  *first = first_value;
-}
+inline void epilogue<0>(ContiguousIterator, ContiguousIterator,
+                        ContiguousIterator::value_type) {}
+
 template <size_t N>
 inline void epilogue(ContiguousIterator first, ContiguousIterator last,
                      ContiguousIterator::value_type first_value) {
   if (distance(first, last) >= N) {
-    using V = datapar<ContiguousIterator::value_type, datapar_abi::fixed_size<N>>;
+    using V = datapar<ContiguousIterator::value_type, abi_for_size_t<N>>;
     const V init = sequence_from_zero<V>() + first_value;
-    store(init, &*first, flags::unaligned);
+    store(init, std::addressof(*first), flags::unaligned);
     first += V::size();
   }
   epilogue<V::size() / 2>(first, last, init[V::size() - 1] + 1);
 }
-void iota(DataparPolicy, ContiguousIterator first, ContiguousIterator last,
+
+void iota(/*!\dataparEPT{}*/, ContiguousIterator first, ContiguousIterator last,
           float first_value) {
-  using V = datapar<ContiguousIterator::value_type>;
+  using V = datapar<ContiguousIterator::value_type, datapar_abi::native>;
   V init = sequence_from_zero<V>() + first_value;
   const V stride = static_cast<float>(V::size());
   for (; distance(first, last) >= V::size(); first += V::size(), init += stride) {
-    store(init, &*first, flags::unaligned);
+    store(init, std::addressof(*first), flags::unaligned);
   }
   epilogue<V::size() / 2>(first, last, init[V::size() - 1] + 1);
 }
