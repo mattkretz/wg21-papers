4bbe5bf3142c1d63dee110de67546c4bbfc4f587
Matthias Kretz <kretz@kde.org>
2017-03-03 16:54:07 +0100

    Add examples section
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ P0214/examples.cpp
@@ -0,0 +1,20 @@
+using floatv = native_datapar<float>;
+void f() {
+  alignas(memory_alignment<floatv>)float data[N];
+  fill_data(data);
+  size_t i = 0;
+  for (; i + floatv::size() <= N; i += floatv::size()) {
+    floatv v(&data[i], flags::vector_aligned);
+    where(v > 100.f, v) = 100.f + (v - 100.f) * 0.1f;
+    v.memstore(&data[i], flags::vector_aligned);
+  }
+  for (; i < N; ++i) {
+    float x = data[i];
+    if (x > 100.f) {
+      x = 100.f + (x - 100.f) * 0.1f;
+    }
+    data[i] = x;
+  }
+}
+
+// ----------------- 8< ---------------------
