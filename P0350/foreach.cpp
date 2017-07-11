16fda1a81cf00bfbbc2b1798ba39a7ddf3aea850
Matthias Kretz <kretz@kde.org>
2017-07-11 16:31:46 +0200

    Update P0350 to C++ WD and drop executors (for now)
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- P0350/foreach.cpp
+++ P0350/foreach.cpp
@@ -8,10 +8,10 @@ template <size_t N>
 inline void epilogue(ContiguousIterator first, ContiguousIterator last,
                      UnaryFunction f) {
   using V = datapar<ContiguousIterator::value_type, abi_for_size_t<N>>;
-  V tmp = load<V>(std::addressof(*first), flags::unaligned);
+  V tmp(std::addressof(*first), flags::element_aligned);
   f(tmp);
   if (is_functor_argument_mutable<UnaryFunction, V>::value) {
-    store(tmp, std::addressof(*first), flags::unaligned);
+    store(tmp, std::addressof(*first), flags::element_aligned);
   }
   epilogue<V::size() / 2>(first, last, f);
 }
@@ -20,10 +20,10 @@ void for_each(/*!\dataparEPT            {}*/, ContiguousIterator first,
               ContiguousIterator last, UnaryFunction f) {
   using V = datapar<ContiguousIterator::value_type, datapar_abi::native>;
   for (; distance(first, last) >= V::size(); first += V::size()) {
-    V tmp = load<V>(std::addressof(*first), flags::unaligned);
+    V tmp(std::addressof(*first), flags::element_aligned);
     f(tmp);
     if (is_functor_argument_mutable<UnaryFunction, V>::value) {
-      store(tmp, std::addressof(*first), flags::unaligned);
+      store(tmp, std::addressof(*first), flags::element_aligned);
     }
   }
   epilogue<V::size() / 2>(first, last, f);
--- P0350/iota.cpp
+++ P0350/iota.cpp
@@ -10,9 +10,10 @@ template <size_t N>
 inline void epilogue(ContiguousIterator first, ContiguousIterator last,
                      ContiguousIterator::value_type first_value) {
   if (distance(first, last) >= N) {
-    using V = datapar<ContiguousIterator::value_type, abi_for_size_t<N>>;
-    const V init = sequence_from_zero<V>() + first_value;
-    store(init, std::addressof(*first), flags::unaligned);
+    using T = ContiguousIterator::value_type;
+    using V = datapar<T, abi_for_size_t<N>>;
+    const V init = V([&](auto i) { return T(i); }) + first_value;
+    store(init, std::addressof(*first), flags::element_aligned);
     first += V::size();
   }
   epilogue<V::size() / 2>(first, last, init[V::size() - 1] + 1);
@@ -20,11 +21,12 @@ inline void epilogue(ContiguousIterator first, ContiguousIterator last,
 
 void iota(/*!\dataparEPT{}*/, ContiguousIterator first, ContiguousIterator last,
           float first_value) {
-  using V = datapar<ContiguousIterator::value_type, datapar_abi::native>;
-  V init = sequence_from_zero<V>() + first_value;
-  const V stride = static_cast<float>(V::size());
+  using T = ContiguousIterator::value_type;
+  using V = datapar<T, datapar_abi::native>;
+  V init = V([&](auto i) { return T(i); }) + first_value;
+  const V stride = T(V::size());
   for (; distance(first, last) >= V::size(); first += V::size(), init += stride) {
-    store(init, std::addressof(*first), flags::unaligned);
+    store(init, std::addressof(*first), flags::element_aligned);
   }
   epilogue<V::size() / 2>(first, last, init[V::size() - 1] + 1);
 }
