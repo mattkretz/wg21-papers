d52f1fe6fbec9d58a234a6e5cc0a4d85610647df
Matthias Kretz <kretz@kde.org>
2017-08-17 14:19:39 +0200

    Change according to P0214R5 and release P0350R1
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- P0350/foreach.cpp
+++ P0350/foreach.cpp
@@ -7,7 +7,7 @@ inline void epilogue<0>(ContiguousIterator, ContiguousIterator, UnaryFunction) {
 template <size_t N>
 inline void epilogue(ContiguousIterator first, ContiguousIterator last,
                      UnaryFunction f) {
-  using V = datapar<ContiguousIterator::value_type, abi_for_size_t<N>>;
+  using V = simd<ContiguousIterator::value_type, abi_for_size_t<N>>;
   V tmp(std::addressof(*first), flags::element_aligned);
   f(tmp);
   if (is_functor_argument_mutable<UnaryFunction, V>::value) {
@@ -16,9 +16,9 @@ inline void epilogue(ContiguousIterator first, ContiguousIterator last,
   epilogue<V::size() / 2>(first, last, f);
 }
 
-void for_each(/*!\dataparEPT            {}*/, ContiguousIterator first,
+void for_each(/*!\simdEPT            {}*/, ContiguousIterator first,
               ContiguousIterator last, UnaryFunction f) {
-  using V = datapar<ContiguousIterator::value_type, datapar_abi::native>;
+  using V = simd<ContiguousIterator::value_type, simd_abi::native>;
   for (; distance(first, last) >= V::size(); first += V::size()) {
     V tmp(std::addressof(*first), flags::element_aligned);
     f(tmp);
--- P0350/iota.cpp
+++ P0350/iota.cpp
@@ -11,7 +11,7 @@ inline void epilogue(ContiguousIterator first, ContiguousIterator last,
                      ContiguousIterator::value_type first_value) {
   if (distance(first, last) >= N) {
     using T = ContiguousIterator::value_type;
-    using V = datapar<T, abi_for_size_t<N>>;
+    using V = simd<T, abi_for_size_t<N>>;
     const V init = V([&](auto i) { return T(i); }) + first_value;
     store(init, std::addressof(*first), flags::element_aligned);
     first += V::size();
@@ -19,10 +19,10 @@ inline void epilogue(ContiguousIterator first, ContiguousIterator last,
   epilogue<V::size() / 2>(first, last, init[V::size() - 1] + 1);
 }
 
-void iota(/*!\dataparEPT{}*/, ContiguousIterator first, ContiguousIterator last,
+void iota(/*!\simdEPT{}*/, ContiguousIterator first, ContiguousIterator last,
           float first_value) {
   using T = ContiguousIterator::value_type;
-  using V = datapar<T, datapar_abi::native>;
+  using V = simd<T, simd_abi::native>;
   V init = V([&](auto i) { return T(i); }) + first_value;
   const V stride = T(V::size());
   for (; distance(first, last) >= V::size(); first += V::size(), init += stride) {
