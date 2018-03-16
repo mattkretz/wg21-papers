59f0014df056a1a860ff3fd19d2533c220e3ce66
Matthias Kretz <kretz@kde.org>
2018-03-16 03:13:00 +0100

    Add new reference wording
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ P0214/reference.cpp
@@ -0,0 +1,29 @@
+class reference  // exposition only
+{
+public:
+    reference(const reference &) = delete;
+
+    operator value_type() const noexcept;
+
+    template <class U> reference operator=(U&& x) &&;
+
+    template <class U> reference operator+=(U&& x) &&;
+    template <class U> reference operator-=(U&& x) &&;
+    template <class U> reference operator*=(U&& x) &&;
+    template <class U> reference operator/=(U&& x) &&;
+    template <class U> reference operator%=(U&& x) &&;
+    template <class U> reference operator|=(U&& x) &&;
+    template <class U> reference operator&=(U&& x) &&;
+    template <class U> reference operator^=(U&& x) &&;
+    template <class U> reference operator<<=(U&& x) &&;
+    template <class U> reference operator>>=(U&& x) &&;
+
+    reference  operator++() &&;
+    value_type operator++(int) &&;
+    reference  operator--() &&;
+    value_type operator--(int) &&;
+
+    friend void swap(reference&& a, reference&& b) noexcept;
+    friend void swap(value_type& a, reference&& b) noexcept;
+    friend void swap(reference&& a, value_type& b) noexcept;
+};
